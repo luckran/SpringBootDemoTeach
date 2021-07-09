5d49ba007cd688ad1ab8f26f0e458a0a96995059
luckran <luckran@outlook.com>
2021-07-09 15:55:59 +0800

    初始化SpringBoot项目

---
+++ src/main/java/com/jd/test/ccbengkaka/CcbengkakaApplication.java
@@ -0,0 +1,13 @@
+package com.jd.test.ccbengkaka;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CcbengkakaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CcbengkakaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jd/test/ccbengkaka/CcbengkakaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jd.test.ccbengkaka;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CcbengkakaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
